d9f7b6faa372b03074fef89891c75d9d3f99344a
gaofeng <861652039@qq.com>
2018-04-03 16:57:09 +0800

    4.3 下午：百度语音合成接入

--- app/src/main/java/com/example/gaofeng/tulingdemo/util/FileUtil.java
+++ app/src/main/java/com/example/gaofeng/tulingdemo/util/FileUtil.java
@@ -1,6 +1,8 @@
 package com.example.gaofeng.tulingdemo.util;
 
+import android.content.Context;
 import android.content.res.AssetManager;
+import android.os.Environment;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -13,6 +15,24 @@ import java.io.InputStream;
 
 public class FileUtil {
 
+    // 创建一个临时目录，用于复制临时文件，如assets目录下的离线资源文件
+    public static String createTmpDir(Context context) {
+        String sampleDir = "baiduTTS";
+        String tmpDir = Environment.getExternalStorageDirectory().toString() + "/" + sampleDir;
+        if (!FileUtil.makeDir(tmpDir)) {
+            tmpDir = context.getExternalFilesDir(sampleDir).getAbsolutePath();
+            if (!FileUtil.makeDir(sampleDir)) {
+                throw new RuntimeException("create model resources dir failed :" + tmpDir);
+            }
+        }
+        return tmpDir;
+    }
+
+    public static boolean fileCanRead(String filename) {
+        File f = new File(filename);
+        return f.canRead();
+    }
+
     public static boolean makeDir(String dirPath) {
         File file = new File(dirPath);
         if (!file.exists()) {
@@ -22,26 +42,9 @@ public class FileUtil {
         }
     }
 
-    public static String getContentFromAssetsFile(AssetManager assets, String source) {
-        InputStream is = null;
-        FileOutputStream fos = null;
-        String result = "";
-        try {
-            is = assets.open(source);
-            int lenght = is.available();
-            byte[] buffer = new byte[lenght];
-            is.read(buffer);
-            result = new String(buffer, "utf8");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return result;
-    }
-
-    public static boolean copyFromAssets(AssetManager assets, String source, String dest,
-                                         boolean isCover) throws IOException {
+    public static void copyFromAssets(AssetManager assets, String source, String dest, boolean isCover)
+            throws IOException {
         File file = new File(dest);
-        boolean isCopyed = false;
         if (isCover || (!isCover && !file.exists())) {
             InputStream is = null;
             FileOutputStream fos = null;
@@ -54,7 +57,6 @@ public class FileUtil {
                 while ((size = is.read(buffer, 0, 1024)) >= 0) {
                     fos.write(buffer, 0, size);
                 }
-                isCopyed = true;
             } finally {
                 if (fos != null) {
                     try {
@@ -66,8 +68,6 @@ public class FileUtil {
                     }
                 }
             }
-
         }
-        return isCopyed;
     }
 }
--- app/src/main/java/com/example/gaofeng/tulingdemo/view/activity/MainActivity.java
+++ app/src/main/java/com/example/gaofeng/tulingdemo/view/activity/MainActivity.java
@@ -15,8 +15,16 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.baidu.tts.chainofresponsibility.logger.LoggerProxy;
+import com.baidu.tts.client.SpeechSynthesizer;
+import com.baidu.tts.client.SpeechSynthesizerListener;
+import com.baidu.tts.client.TtsMode;
 import com.example.gaofeng.tulingdemo.R;
+import com.example.gaofeng.tulingdemo.control.InitConfig;
 import com.example.gaofeng.tulingdemo.control.MyRecognizer;
+import com.example.gaofeng.tulingdemo.control.MySyntherizer;
+import com.example.gaofeng.tulingdemo.control.NonBlockSyntherizer;
+import com.example.gaofeng.tulingdemo.listener.UiMessageListener;
 import com.example.gaofeng.tulingdemo.model.bean.DishBean;
 import com.example.gaofeng.tulingdemo.model.bean.NewsBean;
 import com.example.gaofeng.tulingdemo.model.bean.SpeechKnownBean;
@@ -30,9 +38,9 @@ import com.example.gaofeng.tulingdemo.recognization.CommonRecogParams;
 import com.example.gaofeng.tulingdemo.recognization.MessageStatusRecogListener;
 import com.example.gaofeng.tulingdemo.recognization.offline.OfflineRecogParams;
 import com.example.gaofeng.tulingdemo.recognization.online.OnlineRecogParams;
-import com.example.gaofeng.tulingdemo.util.LocationUtil;
 import com.example.gaofeng.tulingdemo.util.Logger;
 import com.example.gaofeng.tulingdemo.util.NetWorkUtil;
+import com.example.gaofeng.tulingdemo.util.OfflineResource;
 import com.example.gaofeng.tulingdemo.util.StringUtil;
 import com.example.gaofeng.tulingdemo.view.fragment.TextFragment;
 import com.example.gaofeng.tulingdemo.view.ui.BaiduASRDigitalDialog;
@@ -41,12 +49,12 @@ import com.example.gaofeng.tulingdemo.view.ui.SimpleTransApplication;
 import com.google.gson.Gson;
 
 import org.greenrobot.eventbus.EventBus;
-import org.greenrobot.eventbus.Subscribe;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -100,6 +108,31 @@ public class MainActivity extends AppCompatActivity {
     protected boolean enableOffline = false;
     protected boolean running = false;
 
+    // ================== 初始化参数设置开始 ==========================
+    /**
+     * 发布时请替换成自己申请的appId appKey 和 secretKey。注意如果需要离线合成功能,请在您申请的应用中填写包名。
+     * 本demo的包名是com.baidu.tts.sample，定义在build.gradle中。
+     */
+    protected String appId = "11034666";
+
+    protected String appKey = "HqhwIAVMaO753SC0of9q9Gqt";
+
+    protected String secretKey = "jTfb3xWWtcIGs1z20U0Ec6xUf16aOY2v";
+
+    // TtsMode.MIX; 离在线融合，在线优先； TtsMode.ONLINE 纯在线； 没有纯离线
+    protected TtsMode ttsMode = TtsMode.MIX;
+
+    // 离线发音选择，VOICE_FEMALE即为离线女声发音。
+    // assets目录下bd_etts_common_speech_m15_mand_eng_high_am-mix_v3.0.0_20170505.dat为离线男声模型；
+    // assets目录下bd_etts_common_speech_f7_mand_eng_high_am-mix_v3.0.0_20170512.dat为离线女声模型
+    protected String offlineVoice = OfflineResource.VOICE_FEMALE;
+
+    // ===============初始化参数设置完毕，更多合成参数请至getParams()方法中设置 =================
+
+    // 主控制类，所有合成控制方法从这个类开始
+    protected MySyntherizer synthesizer;
+
+    //语音识别handler
     private Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
@@ -115,6 +148,14 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     };
+    //语音合成handler
+    private Handler mainHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            Log.e("msg", String.valueOf(msg.obj));
+        }
+    };
 
 
     @Override
@@ -123,7 +164,87 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         initRecog();
+        initialTts();
     }
+
+
+    /**
+     * 初始化引擎，需要的参数均在InitConfig类里
+     * <p>
+     * DEMO中提供了3个SpeechSynthesizerListener的实现
+     * MessageListener 仅仅用log.i记录日志，在logcat中可以看见
+     * UiMessageListener 在MessageListener的基础上，对handler发送消息，实现UI的文字更新
+     * FileSaveListener 在UiMessageListener的基础上，使用 onSynthesizeDataArrived回调，获取音频流
+     */
+    protected void initialTts() {
+        LoggerProxy.printable(true); // 日志打印在logcat中
+        // 设置初始化参数
+        // 此处可以改为 含有您业务逻辑的SpeechSynthesizerListener的实现类
+        SpeechSynthesizerListener listener = new UiMessageListener(mainHandler);
+
+        Map<String, String> params = getParams();
+        // appId appKey secretKey 网站上您申请的应用获取。注意使用离线合成功能的话，需要应用中填写您app的包名。包名在build.gradle中获取。
+        InitConfig initConfig = new InitConfig(appId, appKey, secretKey, ttsMode, params, listener);
+
+        // 如果您集成中出错，请将下面一段代码放在和demo中相同的位置，并复制InitConfig 和 AutoCheck到您的项目中
+        synthesizer = new NonBlockSyntherizer(this, initConfig, mainHandler); // 此处可以改为MySyntherizer 了解调用过程
+    }
+
+
+    /**
+     * 合成的参数，可以初始化时填写，也可以在合成前设置。
+     *
+     * @return
+     */
+    protected Map<String, String> getParams() {
+        Map<String, String> params = new HashMap<String, String>();
+        // 以下参数均为选填
+        // 设置在线发声音人： 0 普通女声（默认） 1 普通男声 2 特别男声 3 情感男声<度逍遥> 4 情感儿童声<度丫丫>
+        params.put(SpeechSynthesizer.PARAM_SPEAKER, "0");
+        // 设置合成的音量，0-9 ，默认 5
+        params.put(SpeechSynthesizer.PARAM_VOLUME, "9");
+        // 设置合成的语速，0-9 ，默认 5
+        params.put(SpeechSynthesizer.PARAM_SPEED, "5");
+        // 设置合成的语调，0-9 ，默认 5
+        params.put(SpeechSynthesizer.PARAM_PITCH, "5");
+
+        params.put(SpeechSynthesizer.PARAM_MIX_MODE, SpeechSynthesizer.MIX_MODE_DEFAULT);
+        // 该参数设置为TtsMode.MIX生效。即纯在线模式不生效。
+        // MIX_MODE_DEFAULT 默认 ，wifi状态下使用在线，非wifi离线。在线状态下，请求超时6s自动转离线
+        // MIX_MODE_HIGH_SPEED_SYNTHESIZE_WIFI wifi状态下使用在线，非wifi离线。在线状态下， 请求超时1.2s自动转离线
+        // MIX_MODE_HIGH_SPEED_NETWORK ， 3G 4G wifi状态下使用在线，其它状态离线。在线状态下，请求超时1.2s自动转离线
+        // MIX_MODE_HIGH_SPEED_SYNTHESIZE, 2G 3G 4G wifi状态下使用在线，其它状态离线。在线状态下，请求超时1.2s自动转离线
+
+        // 离线资源文件， 从assets目录中复制到临时目录，需要在initTTs方法前完成
+        OfflineResource offlineResource = createOfflineResource(offlineVoice);
+        // 声学模型文件路径 (离线引擎使用), 请确认下面两个文件存在
+        params.put(SpeechSynthesizer.PARAM_TTS_TEXT_MODEL_FILE, offlineResource.getTextFilename());
+        params.put(SpeechSynthesizer.PARAM_TTS_SPEECH_MODEL_FILE,
+                offlineResource.getModelFilename());
+        return params;
+    }
+
+    protected OfflineResource createOfflineResource(String voiceType) {
+        OfflineResource offlineResource = null;
+        try {
+            offlineResource = new OfflineResource(this, voiceType);
+        } catch (IOException e) {
+            // IO 错误自行处理
+            e.printStackTrace();
+        }
+        return offlineResource;
+    }
+
+
+    /**
+     * speak 实际上是调用 synthesize后，获取音频流，然后播放。
+     * 获取音频流的方式见SaveFileActivity及FileSaveListener
+     * 需要合成的文本text的长度不能超过1024个GBK字节。
+     */
+    private void speak(String text) {
+        synthesizer.speak(text);
+    }
+
 
     protected void initRecog() {
         listener = new ChainRecogListener();
@@ -191,6 +312,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        synthesizer.release();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        synthesizer.stop();
     }
 
     @OnClick(R.id.btn_search)
@@ -235,6 +363,7 @@ public class MainActivity extends AppCompatActivity {
                                 transaction.replace(R.id.container, tf);
                                 transaction.commitAllowingStateLoss();
                                 EventBus.getDefault().postSticky(new TextMsg(textBean.getText()));
+                                speak(textBean.getText());
 
                                 break;
                             case 308000:
